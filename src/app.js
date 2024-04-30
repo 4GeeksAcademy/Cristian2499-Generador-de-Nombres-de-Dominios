5af2f83e38db4840afda584031d4397616823d06
Cristian2499 <162387827+Cristian2499@users.noreply.github.com>
2024-04-30 03:31:44 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const dominio = document.getElementById("dominio");
+  function domainGenerator() {
+    let pronouns = ["the", "our"];
+    let adjectives = ["great", "big"];
+    let nouns = ["jogger", "racoon"];
+
+    for (let pronoun of pronouns) {
+      for (let adj of adjectives) {
+        for (let noun of nouns) {
+          let result = `<p>${pronoun}${adj}${noun}.com</p>`;
+          dominio.innerHTML += result;
+        }
+      }
+    }
+  }
+  domainGenerator();
 };
